4017df61a83e8bd055228b915faad9cd64949189
Gokul-T-M <gokulmanoharan2805@gmail.com>
2025-03-30 23:50:21 +0530

    Added C# assignment 2 and some improvements in assignment 1

--- Practice Assignments/C# Practice Assignments/Assignment1/Assignment1/Program.cs
+++ Practice Assignments/C# Practice Assignments/Assignment1/Assignment1/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
 
-
             bool Flag = true;
 
             while (Flag)
             {
                 Console.WriteLine();
+                Console.WriteLine("-----------------------------------------------------------------------------------");
                 Console.WriteLine("Please choose a Question Number: ");
                 Console.WriteLine();
                 Console.WriteLine("1.Write a C# Sharp program to accept two integers and check whether they are equal or not.");
@@ -22,7 +22,13 @@
                 Console.WriteLine();
 
 
-                switch (Convert.ToInt32(Console.ReadLine()))
+                if(!int.TryParse(Console.ReadLine(), out int Choice))
+                {
+                    Console.WriteLine("---- Please enter a valid number between 1 and 6 ----");
+                    continue;
+                }
+
+                switch (Choice)
                 {
                     case 1:
                         CheckEqual();
@@ -53,15 +59,8 @@
                         break;
 
                     }
-
-
-
-
-
-
-
-
                 }
+
                 static void CheckEqual()
                 {
                     Console.WriteLine("Enter First number A: ");
@@ -75,14 +74,14 @@
                     }
                     else Console.WriteLine($"{A} and {B} are Not Equal");
 
-
                 }
 
 
                 static void CheckPositive()
                 {
                     Console.WriteLine("Enter a Number: ");
-                    int A = int.Parse(Console.ReadLine());
+                    int A = Convert.ToInt32(Console.ReadLine());
+
                     if (A > 0)
                     {
                         Console.WriteLine($"{A} is Positive");
@@ -94,13 +93,14 @@
                     else Console.WriteLine($"{A} is Negative");
                 }
 
+
                 static void Operations()
                 {
                     Console.WriteLine("Enter Number A: ");
                     int A = Convert.ToInt32(Console.ReadLine());
 
                     Console.WriteLine("Enter Operator: ");
-                    char Operator = Convert.ToChar(Console.Read());
+                    char Operator = Convert.ToChar(Console.ReadLine());
 
                     Console.WriteLine("Enter Number B: ");
                     int B = Convert.ToInt32(Console.ReadLine());
@@ -128,8 +128,11 @@
                             break;
                     }
                 }
+
+
                 static void MultiplicationTable()
                 {
+
                     Console.WriteLine("Enter the Number to be Multiplied");
                     int Num = Convert.ToInt32(Console.ReadLine());
 
@@ -140,14 +143,15 @@
                     {
                         Console.WriteLine($"{i}*{Num} = {i * Num}");
                     }
+
                 }
 
                 static void CustomSum()
                 {
                     Console.WriteLine("Enter number A: ");
-                    int A = Convert.ToInt32(Console.ReadLine);
+                    int A = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Enter number B: ");
-                    int B = Convert.ToInt32(Console.ReadLine);
+                    int B = Convert.ToInt32(Console.ReadLine());
 
                     if (A == B) Console.WriteLine($"The custom Sum of {A} and {B} is: {(A + B) * 3}");
                     else Console.WriteLine($"The Custom Sum of {A} and {B} is: {A + B}");
--- Practice Assignments/C# Practice Assignments/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Practice Assignments/C# Practice Assignments/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef6a0362e97aa98c5ebf068ec4057bd58258aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
